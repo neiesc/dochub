0521d8ecaf88da19d5cc90082e4ac3ea15efd978
neiesc <edinei3a@hotmail.com>
2013-08-31 02:21:03 -0300

    Add logo node.js and top menu alphabetical order

--- static/js/settings.js
+++ static/js/settings.js
@@ -9,9 +9,17 @@ define([], function(){
       { lang: 'php', name: 'PHP' },
       { lang: 'python', name: 'Python' },
       { lang: 'python3', name: 'Python3' },
-      { lang: 'python3', name: 'Python3' },
       { lang: 'nodejs', name: 'Node.js' }
     ]
   }
+  
+  Settings.languages.sort(function(a, b){
+    var nameA=a.name.toLowerCase(), nameB=b.name.toLowerCase()
+    //sort string ascending
+    if (nameA < nameB) { return -1 }
+    if (nameA > nameB) { return 1 }
+    return 0 //default return value (no sorting)
+  });
+  
   return Settings;
 });
